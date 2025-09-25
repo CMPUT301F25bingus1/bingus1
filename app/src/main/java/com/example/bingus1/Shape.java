d828ce839a75b49857fe45b0671d5bae9ab76ad2
chiu4 <jadenchiu16@gmail.com>
2025-09-25 17:33:31 -0600

    Shape colour

--- app/src/main/java/com/example/bingus1/Shape.java
+++ app/src/main/java/com/example/bingus1/Shape.java
@@ -3,7 +3,7 @@ package com.example.bingus1;
 public abstract class Shape {
     protected int x;
     protected int y;
-
+    String colour = "teal";
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
