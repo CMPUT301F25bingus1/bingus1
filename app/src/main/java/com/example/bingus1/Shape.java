5459fe8d3e515186322d1bf802b73dee6120be6d
Yong Lee <gjftns7220@gmail.com>
2025-09-25 17:27:59 -0600

    color

--- app/src/main/java/com/example/bingus1/Shape.java
+++ app/src/main/java/com/example/bingus1/Shape.java
@@ -3,7 +3,7 @@ package com.example.bingus1;
 public abstract class Shape {
     protected int x;
     protected int y;
-
+    String color = "indigo";
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
