cd11ce886a75adad20d7cf43f9b3e06ea8b4d5e8
Zoe <zprefont@ualberta.ca>
2025-09-25 18:14:21 -0600

    fart green

--- app/src/main/java/com/example/bingus1/Shape.java
+++ app/src/main/java/com/example/bingus1/Shape.java
@@ -4,11 +4,7 @@ public abstract class Shape {
     protected int x;
     protected int y;
 
-<<<<<<< HEAD
-    String colour = "fart green2";
-=======
     String color = "Fart Green1";
->>>>>>> 2bbb15d7e55bc0a63ef2abb8aaeeabfb89dc9e33
 
     public Shape(int x, int y) {
         this.x = x;
