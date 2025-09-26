129e37341536638f59aafe7e53a5b491fc052716
Zoe <zprefont@ualberta.ca>
2025-09-25 18:07:22 -0600

    added fart green 2

--- app/src/main/java/com/example/bingus1/Shape.java
+++ app/src/main/java/com/example/bingus1/Shape.java
@@ -3,7 +3,8 @@ package com.example.bingus1;
 public abstract class Shape {
     protected int x;
     protected int y;
-    
+
+    String colour = "fart green2";
 
     public Shape(int x, int y) {
         this.x = x;
